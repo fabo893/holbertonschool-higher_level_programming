6eae9fedf37427d385e72d45d3d83c6c6e864916
fabo893 <jose.rosa893@gmail.com>
2020-12-08 03:29:05 +0000

    checks cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks a singly linked list.
+ * @list - pointer.
+ * Return: On success, always 0.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
