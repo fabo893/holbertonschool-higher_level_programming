a7f942479377bfe153a01049f9539aa5fc980c52
fabo893 <jose.rosa893@gmail.com>
2020-12-09 03:58:02 +0000

    reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(-122, -96):
     if abs(num) % 2 == 1:
-            num = abs(num) - 32
+        num = abs(num) - 32
     print("{}".format(chr(abs(num))), end="")
