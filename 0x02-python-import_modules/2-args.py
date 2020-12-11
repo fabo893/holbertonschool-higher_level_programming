c1487176f900ed1be56d65b5600d52d4ea4f64fa
fabo893 <jose.rosa893@gmail.com>
2020-12-11 02:25:35 +0000

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arg = sys.argv
+    args = sys.argv
     argu = len(arg)
     if argu == 1:
         print("0 arguments.")
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(argu - 1))
     for a in range(1, argu):
-        print("{:d}: {:S}".format(a, arg[a]))
+        print("{:d}: {:S}".format(a, args[a]))
