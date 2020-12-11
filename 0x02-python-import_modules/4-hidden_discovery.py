7c3a8f4dc22abb790b093533291b97d01ad546ce
fabo893 <jose.rosa893@gmail.com>
2020-12-11 02:43:17 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    y = dir(hidden_4)
-    for x in y:
+    name = dir(hidden_4)
+    for x in name:
         if x[:2] != "__":
-        print("{}".format(x))
+            print("{}".format(x))
