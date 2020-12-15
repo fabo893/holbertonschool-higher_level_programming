a9a2330ce615a0b404f38cadad4f460cde536bd8
fabo893 <jose.rosa893@gmail.com>
2020-12-15 23:40:32 +0000

    changing the returns

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     sec_list = my_list[:]
     if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
-        return my_list
+        return sec_list
+    elif idx > len(my_list):
+        return sec_list
     else:
         for i in my_list:
             if i == idx:
