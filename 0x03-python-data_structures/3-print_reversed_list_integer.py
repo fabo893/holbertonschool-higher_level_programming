3d6a1b32766f3214da9760aac16a6440f281742b
fabo893 <jose.rosa893@gmail.com>
2020-12-15 23:59:21 +0000

    change '=' to 'is'

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     lon = len(my_list) - 1
-    if my_list == None:
+    if my_list is None:
         return
     for idx in my_list:
         if idx < 0:
