250c0744d2bfb01a7580e2d3c661e9d0ece312ec
fabo893 <jose.rosa893@gmail.com>
2020-12-15 17:40:26 +0000

    adding if statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     else:
         for i in range(len(my_list)):
-            my_list[idx] = element
-            return my_list
+            if i == idx:
+                my_list[idx] = element
+                return my_list
