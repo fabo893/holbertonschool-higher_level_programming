1ea46d8f1cfa03e796f3cc0293006d82e74a3974
fabo893 <jose.rosa893@gmail.com>
2020-12-15 18:33:58 +0000

    adding an if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     lon = len(my_list) - 1
     for idx in my_list:
+        if idx < 0:
+            return
         print("{:d}".format(my_list[lon]))
         lon -= 1
