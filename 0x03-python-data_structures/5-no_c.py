87f04c4d9f0ef02134db2e4ef2450d5e047bdc1b
fabo893 <jose.rosa893@gmail.com>
2020-12-16 02:34:48 +0000

    change the if statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     str = my_string[:]
     str2 = ""
     for i in range(len(str)):
-        if i is not 'c' and not 'C':
+        if str[i] is not 'c' and str[i] is not 'C':
             str2 = str[i]
     return str2
