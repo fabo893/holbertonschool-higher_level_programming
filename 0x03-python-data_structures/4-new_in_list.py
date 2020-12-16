43ab06ed1abc238723a7db82c4a95d64775ca0fd
fabo893 <jose.rosa893@gmail.com>
2020-12-16 14:54:49 +0000

    change the statement

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     sec_list = my_list[:]
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return sec_list
-    else:
-        sec_list[idx] = element
+    sec_list[idx] = element
     return sec_list
