bffa55298dd24b9f5bb9ad574e8cb66b1885cf49
fabo893 <jose.rosa893@gmail.com>
2020-12-16 02:28:51 +0000

    adding index to my_string and change the return value

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     str = ""
     for i in range(len(my_string)):
         if i is not 'c' and not 'C':
-            i = my_string
-    return i
+            i = my_string[i]
+    return str
