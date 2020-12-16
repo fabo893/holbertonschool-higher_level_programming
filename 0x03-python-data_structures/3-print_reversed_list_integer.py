8b570017e71a66f27273f290c4748aef4d9acd28
fabo893 <jose.rosa893@gmail.com>
2020-12-16 00:12:38 +0000

    adding two semicolon for the position

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,8 +3,7 @@ def print_reversed_list_integer(my_list=[]):
     lon = len(my_list) - 1
     if my_list is None:
         return
-    for idx in my_list[::-1]:
-        if idx < 0:
-            return
-        print("{:d}".format(my_list[lon]))
-        lon -= 1
+    else:
+        for idx in my_list[::-1]:
+            print("{:d}".format(my_list[idx]))
+            lon -= 1
