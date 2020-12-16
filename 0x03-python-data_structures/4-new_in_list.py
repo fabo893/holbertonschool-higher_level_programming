c166de1602a74fb67d1ff109f14b9289cf698a3f
fabo893 <jose.rosa893@gmail.com>
2020-12-16 14:45:01 +0000

    reorganize the last 'return'

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,6 @@ def new_in_list(my_list, idx, element):
     sec_list = my_list[:]
     if idx < 0:
         return sec_list
-    elif idx > len(my_list):
-        return sec_list
     else:
         sec_list[idx] = element
-        return sec_list
+    return sec_list
