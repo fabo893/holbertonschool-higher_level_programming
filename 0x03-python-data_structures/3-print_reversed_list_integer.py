7a480828eb306a067a4b3c6e6319e8f8194d7d0f
fabo893 <jose.rosa893@gmail.com>
2020-12-16 00:20:16 +0000

    removing var (dont used)

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    lon = len(my_list) - 1
     if my_list is None:
         return
     else:
         for idx in my_list[::-1]:
-            print("{:d}".format(my_list[idx]))
-            lon -= 1
+            print("{:d}".format(idx))
