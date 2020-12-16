786ff9df0ff82e92f01d022026a2966811256e47
fabo893 <jose.rosa893@gmail.com>
2020-12-16 13:49:11 +0000

    adding an if statement for 'matrix[0]'

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     idx = 1
-    for i in matrix:
-        for x in i:
-            if idx < len(i):
-                print(x, end=' ')
-            else:
-                print(x)
-            idx += 1
-        idx = 1
+    if len(matrix[0]) == 0:
+       print("")
+    else:
+        for i in matrix:
+            for x in i:
+                if idx < len(i):
+                    print("{:d}".format(x), end=" ")
+                else:
+                    print("{:d}".format(x))
+                idx += 1
+            idx = 1
