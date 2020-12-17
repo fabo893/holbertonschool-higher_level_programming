0a492c41051ae0f2863f30144223eb8bb5809f16
fabo893 <jose.rosa893@gmail.com>
2020-12-17 03:19:32 +0000

    fixing for negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    tmp = 0
     if len(my_list) == 0:
         return None
     else:
+        tmp = my_list[0]
         for idx in range(0, len(my_list)):
             if my_list[idx] > tmp:
                 tmp = my_list[idx]
