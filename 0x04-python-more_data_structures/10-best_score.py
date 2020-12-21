d754685824dc6dadfb210e464b2d35fe4f8de75c
fabo893 <jose.rosa893@gmail.com>
2020-12-21 03:29:57 +0000

    elif statement added

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    elif len(a_dictionary) == 0:
+        return None
     else:
         score = 0
         idx = 0
