04b967d88ef7abfb8dec3995a2c0b4cb0230f925
fabo893 <jose.rosa893@gmail.com>
2021-01-06 04:17:17 +0000

    writing again cause something was happen

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """2-square.py
-This is a module documentation
+This is a module documenatation
 """
 
 
 class Square:
     """This class uses a private attribute"""
-    def ___init___(self, size=0):
+    def __init__(self, size=0):
         """Method __init__"""
-        if (type(size) is not int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if (size < 0):
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
