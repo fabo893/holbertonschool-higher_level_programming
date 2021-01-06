24d0ffad44428b14bbcae31a69abbba5cb327959
fabo893 <jose.rosa893@gmail.com>
2021-01-06 03:45:33 +0000

    adding a private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""1-square.py
+This a module documentation
+"""
+
+
+class Square:
+    """This class uses a private instance"""
+    def __init__(self, size):
+        """Method __init__"""
+        self.__size = size
