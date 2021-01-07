c56e57a1a364be74ef3afe6547cdb88607696867
fabo893 <jose.rosa893@gmail.com>
2021-01-07 17:46:41 +0000

    fixed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """4-square.py
 This is a module documentation
 """
@@ -9,11 +8,7 @@ class Square:
     """This is a class documentation"""
     def __init__(self, size=0):
         """Method __init__"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         """Method that return the area"""
@@ -22,8 +17,17 @@ class Square:
 
     @property
     def size(self):
+        """Getter method for size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Args:
+             value (int): new int to be set
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
