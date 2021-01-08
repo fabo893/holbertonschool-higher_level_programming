a47b2dd8da87828ed8dab582dd4304f5ff27b32f
fabo893 <jose.rosa893@gmail.com>
2021-01-08 03:26:03 +0000

    fixing the except statement

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(value):
         if type(value) is int:
             print("{:d}".format(value))
             return True
-    except TypeError:
+    except:
         return False
