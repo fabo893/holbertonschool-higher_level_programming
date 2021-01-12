1c45308c163494ffaab6fe0d52814e5d84cf2d0e
fabo893 <jose.rosa893@gmail.com>
2021-01-11 23:13:23 -0400

    addint int method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,9 @@ class Rectangle:
     """This class defines a rectangle with width and height"""
     def __init__(self, width=0, height=0):
         """Method __init__"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
+
 
     @property
     def width(self):
@@ -21,7 +22,7 @@ class Rectangle:
         """Setter for width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if int(value) < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -35,6 +36,6 @@ class Rectangle:
         """Setter for height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if int(value) < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
