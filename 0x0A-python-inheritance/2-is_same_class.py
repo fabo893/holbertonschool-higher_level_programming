8bbd1857c9c0461f0d3ea00575c07d2634201b84
fabo893 <jose.rosa893@gmail.com>
2021-01-20 03:26:55 +0000

    changing the statement

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -15,7 +15,7 @@ def is_same_class(obj, a_class):
 
             Return - If is instance return True, otherwise False
     """
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
