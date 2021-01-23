e97670d63f9f3545606635ad759b5125016b64da
fabo893 <jose.rosa893@gmail.com>
2021-01-23 00:08:22 +0000

    ReadMe file, hash header and task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_create - create a hash table
+ * @size - te size of the array
+ *
+ * Return: On success, a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_node_t) * size);
+	if (table == NULL)
+		return (NULL);
+
+	return (table);
+}
