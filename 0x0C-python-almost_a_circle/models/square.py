439e9439738ed84b4d21c14e143cebee40c90105
fabo893 <jose.rosa893@gmail.com>
2021-01-27 18:56:37 +0000

    updating size setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,3 +20,14 @@ class Square(Rectangle):
         return "[{}] ({}) {}/{} - {}".format(
                 self.__class__.__name__, self.id, self.x,
                 self.y, self.size)
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
