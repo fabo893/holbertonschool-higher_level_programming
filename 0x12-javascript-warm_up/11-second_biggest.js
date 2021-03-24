b314d976a318401690f31e85522a5872790b7aed
fabo893 <jose.rosa893@gmail.com>
2021-03-24 04:06:39 +0000

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/nodejs
+if (process.argv.length === 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
+  console.log(0);
+} else {
+  let count = 2;
+  const ar = [];
+  while (count < process.argv.length) {
+	  ar.push(process.argv[count]);
+	  count++;
+  }
+  ar.sort(function (a, b) {
+	  return a - b;
+  });
+  console.log(ar[ar.length - 2]);
+}
