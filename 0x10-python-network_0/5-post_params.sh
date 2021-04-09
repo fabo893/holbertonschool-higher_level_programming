5a3d7845ae61a9e133a1716419544592c2824589
fabo893 <jose.rosa893@gmail.com>
2021-04-09 03:52:32 +0000

    rearrange

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl POST parameters
-curl "$1" -sX POST -d "email=hr@holbertonSchool.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=hr@holbertonSchool.com&subject=I will always be here for PLD" "$1"
