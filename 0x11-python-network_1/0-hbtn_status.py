cc507551fe1961abc454b5844442fff888ba435f
fabo893 <jose.rosa893@gmail.com>
2021-04-14 04:09:49 +0000

    fixing the indentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,8 +10,6 @@ from urllib import request
 if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as rep:
         html = rep.read()
-        print("Body response:\n\
-                \t- type: {}\n\
-                \t- content: {}\n\
-                \t- utf8 content: {}".format(type(html), html,
-                                             html.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
+              format(type(html), html, html.decode('utf-8')))
