e8e670ca4eaafc29b9adce7f024e2814a9bfec66
fabo893 <jose.rosa893@gmail.com>
2021-05-12 04:50:38 +0000

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        x = fct(*args)
+        return x
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
