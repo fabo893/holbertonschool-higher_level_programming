cb13a455d6d5f0de821cd188971336c68004dc4a
fabo893 <jose.rosa893@gmail.com>
2021-05-12 04:28:49 +0000

    Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    import sys
+    try:
+        n = int(value)
+        print("{:d}".format(n))
+        return True
+    except Exception:
+        err = sys.stderr.write
+        err("Exception: Unknown format code 'd' for object of type 'str'\n")
+        return False
