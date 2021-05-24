ef8227f924603002c3b12fcaf7367bfed4ba0428
fabo893 <jose.rosa893@gmail.com>
2021-05-24 21:06:06 +0000

    removing else statement

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,8 +26,7 @@ class Base:
         """the to_json"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
